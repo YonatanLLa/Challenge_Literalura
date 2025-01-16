1f37f07a39c5f568d5e34a27ff5d6c4227b25d77
yonatanlla <yonatanllanto174@gmail.com>
2025-01-16 03:46:45 -0500

    literalura

---
+++ src/main/java/com/alura/literalura/service/ConvierteDatos.java
@@ -0,0 +1,19 @@
+package com.alura.literalura.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConvierteDatos implements IConvierteDatos {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+        try {
+            return objectMapper.readValue(json, clase);
+        } catch (Exception e) {
+            throw new RuntimeException("Error al convertir los datos", e);
+        }
+    }
+}
